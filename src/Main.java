7a939fffa9bd080913f767ce0a01274203cd9503
Jens <zeiclar2@gmail.com>
2018-11-06 11:00:00 +0100

    Jeg har rykket scanneren ned i while loopet.

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,16 @@ public class Main {
     boolean lang_factor = false;
         Scanner scan = new Scanner(System.in);
         System.out.print("Vælg 1 for dansk eller vælg 2 for engelsk: ");
-        language = scan.nextInt();
 
         while (lang_factor == false) {
-
+            language = scan.nextInt();
             if (language == 1) {
                 lang_factor = true;
             } else if (language == 2) {
                 lang_factor = true;
             } else {
                 System.out.println("Dette er ikke gyldigt. Vælg enten 1 for dansk eller 2 for engelsk.");
+
             }
         }
 
