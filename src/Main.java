7da11e901956230c0926acb21c9187bc44541d19
Jens <zeiclar2@gmail.com>
2018-11-06 10:53:32 +0100

    Main med language vælger.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+    int language;
+    boolean lang_factor = false;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Vælg 1 for dansk eller vælg 2 for engelsk: ");
+        language = scan.nextInt();
 
+        while (lang_factor == false) {
 
+            if (language == 1) {
+                lang_factor = true;
+            } else if (language == 2) {
+                lang_factor = true;
+            } else {
+                System.out.println("Dette er ikke gyldigt. Vælg enten 1 for dansk eller 2 for engelsk.");
+            }
+        }
 
     }
 }
