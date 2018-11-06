39a2736c98920ee77599d89e8f9aea1a163a1c3f
kbirongo@gmail.com <mgr85wpf>
2018-11-06 09:51:29 +0100

    Inserting dice class

---
+++ src/Dice.java
@@ -0,0 +1,23 @@
+public class Dice {
+
+    private int sides;
+
+        //constructor to choose the amount of sides of the die
+        public Dice(int sides){
+            this.sides=sides;
+        }
+
+        // roll the die and return the value (1-6)
+        public int roll() {
+            int d1 = (int)(Math.random() * sides + 1);
+            return d1;
+        }
+
+        // roll the die n times and print the values
+        public void rollMultiple(int n) {
+            for (int i = 1; i < n; i++) {
+                System.out.print(roll() + " ");
+            }
+        }
+
+}
