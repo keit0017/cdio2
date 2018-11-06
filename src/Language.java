e117d9d277b7f3ecb57ac61e0fbcec797cfb7096
Joe-Briefcase <s185143@student.dtu.dk>
2018-11-06 11:08:56 +0100

    Language klassen

---
+++ src/Language.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Language {
+    private String[] messages = new String[9];
+    private String file;
+
+    public Language()
+    {
+        String file = "language.txt";
+        this.file = file;
+    }
+
+    public void chooseLang(int choice)
+    {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        for (int i = 0; i < 9; i++)
+        {
+            messages[i] = reader.readLine();
+        }
+        reader.close();
+    }
+}
