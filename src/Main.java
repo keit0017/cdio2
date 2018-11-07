6aae7a114542c48217ae3b2c9a98127e00a08252
Jens <zeiclar2@gmail.com>
2018-11-07 11:27:04 +0100

    Man kan nu navngive spillere.

--- src/Main.java
+++ src/Main.java
@@ -13,11 +13,11 @@ public class Main {
     boolean game = true;
     Language lang = new Language();
     Scanner scan = new Scanner(System.in);
-        System.out.print("vaelg 1 for dansk:  choose 2 for english: ");
+        System.out.print("vælg 1 for dansk:  choose 2 for english: ");
 
         while (!lang_factor) {
             language = scan.nextInt();
-
+            scan.nextLine();
             if (language == 1) {
                 lang.chooseLang(language);
                 lang_factor = true;
@@ -25,24 +25,17 @@ public class Main {
                 lang.chooseLang(language);
                 lang_factor = true;
             } else {
-                System.out.println("This is not a valid answer. vaelg 1 for dansk:  choose 2 for english:");
+                System.out.println("This is not a valid answer. vølg 1 for dansk:  choose 2 for english:");
 
             }
         }
 
-        String[] naming= new String[3];
-
-
-        for(int i=0; i<naming.length;i++){
-            naming[i]=scan.nextLine();
-            System.out.println("Spiller"+i+":");
-
-        }
-
-        Player player1 = new Player((naming[0]));
-        Player player2 = new Player((naming[1]));
-
-
+        System.out.print("navngiv spiller 1");
+        String name = scan.nextLine();
+        Player player1 = new Player(name);
+        System.out.print("navngiv spiller 2");
+        name = scan.nextLine();
+        Player player2 = new Player(name);
 
         while (game){
 
