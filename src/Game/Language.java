e5e0eee90069e1cee6c1c0090a52493b5fc2655d
Joe-Briefcase <s185143@student.dtu.dk>
2018-11-08 12:41:21 +0100

    Har tilføjet test af Account.getPoints() der tester at points ikke kan blive negativ og har lavet packages.

--- src/Game/Language.java
+++ src/Game/Language.java
@@ -1,3 +1,5 @@
+package Game;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
--- src/Game/Player.java
+++ src/Game/Player.java
@@ -1,3 +1,5 @@
+package Game;
+
 public class Player {
 
     private String gamertag;
